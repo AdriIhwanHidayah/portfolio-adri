bd9d81ac54c5b3f0f406c3c609e8defc09e952b5
AdriIhwanHidayah <2300016108@webmail.uad.ac.id>
2026-03-13 11:25:30 +0700

    upgrade portfolio about me

--- portfolio-adri/src/components/About.jsx
+++ portfolio-adri/src/components/About.jsx
@@ -3,10 +3,19 @@ function About() {
     <section id="about" className="section">
       <h2>About Me</h2>
       <p>
-        Mahasiswa Sistem Informasi dengan fokus pada pengembangan perangkat lunak,
-        infrastruktur jaringan, dan analisis sistem. Berpengalaman membangun aplikasi
-        Flutter, menyusun SRS, serta mengimplementasikan VLAN dan routing.
-      </p>
+       Saya adalah mahasiswa Sistem Informasi di Universitas
+        Ahmad Dahlan yang memiliki ketertarikan pada Software Engineering,
+         UI/UX Design, serta pengembangan aplikasi web dan mobile. Saya terbiasa
+          mengerjakan proyek mulai dari analisis kebutuhan pengguna, perancangan desain antarmuka,
+           hingga implementasi sistem.
+      </p> 
+      <p>Beberapa proyek yang telah saya kerjakan meliputi pengembangan aplikasi mobile
+       menggunakan Flutter, perancangan sistem informasi berbasis web, serta penelitian UX
+        untuk meningkatkan pengalaman pengguna pada aplikasi digital. Melalui proyek-proyek tersebut,
+         saya mengembangkan kemampuan dalam problem solving, desain sistem, dan pengembangan perangkat lunak.</p>
+         <p>Saya memiliki minat untuk terus berkembang dalam dunia teknologi dan berkontribusi dalam pengembangan
+           produk digital sebagai UI/UX Designer, Frontend Developer, atau Software Developer.</p>
+  
     </section>
   )
 }
